ec2c6f14a17ff0c762d2edb7a7efd157be71818a
waghanil87 <waghanil87@gmail.com>
2016-04-05 09:33:39 +0530

    corrected code

--- controllers/calculator-controller.js
+++ controllers/calculator-controller.js
@@ -1,117 +1,77 @@
-var app = angular.module('app', []);
-  
-app.controller('CalculatorController', function($scope, CalculatorService, detailService, delFilter) {
-    $scope.calculation="";
-    $scope.result=null;
-    $scope.calNumber="";
-    oldOperator=""
-    $scope.operatorClicked="false"
-    $scope.initialScope = function(){
-        if(detailService.get() == '');
-            $scope.disable = false;
-    }
-    $scope.setFocus = function(){
-        $("#calNumber").focus()
-    }
-    //reset calculator input value
-    $scope.reset=function(){
-        $scope.calNumber=""
-        $scope.setFocus();
-    }
-   //delete calculator input value and result/calculation value
-    $scope.clear=function(){
-        $scope.calNumber=0;
-        $scope.calculation="";
-        $scope.result=null;
-        var index={}
-        detailService.set(index);
-        $scope.setFocus();
-    }
-    //delete last charactor in input
-    $scope.del=function(){
-        $scope.calNumber=delFilter(parseFloat($scope.calNumber));
-    }
-    //Soft keyboard input
-    $scope.numPressed = function(num) {
-        $scope.calNumber = $scope.calNumber +""+num;
-        $scope.calNumber = parseFloat($scope.calNumber);
-        $scope.setFocus();
-        $scope.operatorClicked="false";
-    }
-    $scope.records = [];
-    var inx = 0;
-    var inx;
-    var newval;
-    $scope.calc=$scope.calculation;
-    $scope.storage =function(){
-        $scope.rno=detailService.get();
-         var validObj = Object.getOwnPropertyNames(detailService.get()).length
-        if(validObj<4){//object has 4properties
-            $scope.records.push({recordNo:inx,res:$scope.result,cal:$scope.calculation});
-            inx++; 
-        }
-        else{
-        //UPDATE existing record
-         var answer = confirm("Update data?")
-            if (answer){
-                $scope.index=detailService.get();
-                $scope.index.cal=$scope.calculation;
-                $scope.index.res=$scope.result;
-                var index={}
-                detailService.set(index);
-            }
-            else{
-                $scope.clear()
-            }
-           
-        }
-        console.log($scope.rno)
-        $scope.setFocus();
-        $scope.clear();
-         if($scope.records.length != 0)
-                $scope.disable = true
-    }
-    $scope.delete =function(){
-        if($scope.disable == true){
-            $scope.index=detailService.get();
-            $scope.records.splice($scope.index, 1);
-            if($scope.records.length == 0)
-                $scope.initialScope();
-        }
-        
-    }
-    $scope.savedRecord =function(index){
-        //var row=detailService.get()
-       /* if(detailService.get() == '')
-            $scope.disable = true;*/
-        detailService.set(index);
-        //console.log(Object.getOwnPropertyNames(detailService.get()).length);
-        //detailService.get().getOwnPropertyNames(obj).length
-        $scope.selected = index;
-        $scope.calculation = index.cal;
-        $scope.result = index.res;
-    }
-    $scope.isActive = function(index) {
-        return $scope.selected === index;
-    };
-    $scope.calculate = function(operator){
-     if($scope.operatorClicked=="false"){
-            if($scope.result == null){
-               $scope.result =  parseFloat($scope.calNumber);
-               $scope.calculation = ""+$scope.result;
-               oldOperator = operator;
-            }else if($scope.calNumber == ""){
-                oldOperator = operator;
-            }else{
-                if(oldOperator !='=')
-                    $scope.calculation =  $scope.calculation +" "+ oldOperator +" "+ $scope.calNumber;
-                $scope.result = CalculatorService.maths(parseFloat($scope.result), parseFloat($scope.calNumber), oldOperator);
-                $scope.calNumber = 0; 
-                oldOperator = operator;
-            }
-            $scope.reset();
-            if(oldOperator !='=')
-                $scope.operatorClicked = "true";
-        }
-    }
+ 
+app.service('MathService', function() {
+    this.add = function(a, b) { return a + b };
+    this.subtract = function(a, b) { return a - b };
+    this.multiply = function(a, b) { return a * b };
+    this.divide = function(a, b) { return a / b };
 });
+//define service
+app.service('detailService', function() {
+    var Details='';
+    this.set = function (index) {
+        var validObj = Object.getOwnPropertyNames(index).length
+        if(validObj<4){
+            $('.records a').removeClass('active')
+        }
+        Details= index;
+    },
+
+    this.get = function () {
+        return Details;
+    }
+})
+app.service('CalculatorService', function(MathService){
+    var total;
+       this.maths = function(a,b,operator) { 
+        total=a;
+        //debugger;
+        if(operator=='='){
+            comparision=operator;
+            operator=oldOperator;
+        }
+        if(operator=='+')
+            return MathService.add(a,b);
+        else if (operator=='-')
+            return MathService.subtract(a,b);
+        else if (operator=='*')
+            return MathService.multiply(a,b);
+        else if (operator=='/')
+            return MathService.divide(a,b);
+        else if (operator=='%')
+            return a % b;
+        else if (operator=='log')
+            return Math.log(b);
+        else if (operator=='sin')
+            return Math.sin(b);
+        else if (operator=='cos')
+            return Math.cos(b);
+        else if (operator=='sqrt')
+            return Math.sqrt(b);
+        else if(operator=='^'){
+            console.log(b+"<b")
+            var i=0;
+            for(var i=1; i<b; i++){
+                total=total *a;
+                console.log(a)
+            }
+            return total;
+        }
+        if(comparision == '='){
+            console.log(a)
+            console.log(b)
+            return a;
+        }
+    };
+
+});
+app.service('delFilter', function(){
+    return function(number){
+        var newnumber = number;
+        newnumber=newnumber.toString();
+        newnumber=newnumber.substring(0, newnumber.length-1);
+        if(newnumber.length>0)
+            return newnumber;
+        else
+            return 0;
+    }
+})
